e1a10987a9585cedc3986af116869c57ece677a8
impfundev <ilhammaulana.dev@gmail.com>
2024-07-30 14:59:05 +0700

    fix: layout loan item

--- lib/src/widgets/loans/loan_item.dart
+++ lib/src/widgets/loans/loan_item.dart
@@ -16,141 +16,82 @@ class LoanItem extends StatelessWidget {
     final remainingDays = _loan.remainingDays;
     final bookTitle = _loan.book.title;
 
-    const message = "Are you sure want to Return this book?";
-
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
       child: Card(
-          child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 10.0),
-              child: Badge(
-                label: Text(
-                  remainingDays,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10.0),
+                child: Badge(
+                  label: Text(
+                    remainingDays,
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Expanded(
-                    child: Text(
-                      "Book Title",
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Expanded(
+                      child: Text(
+                        "Book Title",
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: Text(
-                      bookTitle,
-                      textAlign: TextAlign.right,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    Expanded(
+                      child: Text(
+                        bookTitle,
+                        textAlign: TextAlign.right,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Expanded(
+                      child: Text("Loan Date"),
                     ),
-                  )
-                ],
+                    Expanded(
+                      child: Text(
+                        loanDate,
+                        textAlign: TextAlign.right,
+                        style: Theme.of(context).textTheme.labelMedium,
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Expanded(
-                    child: Text("Loan Date"),
-                  ),
-                  Expanded(
-                    child: Text(
-                      loanDate,
-                      textAlign: TextAlign.right,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
+              Padding(
+                padding: const EdgeInsets.only(top: 4.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Expanded(
+                      child: Text("Due Date"),
                     ),
-                  )
-                ],
+                    Expanded(
+                      child: Text(
+                        dueDate,
+                        textAlign: TextAlign.right,
+                        style: Theme.of(context).textTheme.labelMedium,
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Expanded(
-                    child: Text("Due Date"),
-                  ),
-                  Expanded(
-                    child: Text(
-                      dueDate,
-                      textAlign: TextAlign.right,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.only(top: 20.0),
-              child: FilledButton(
-                child: const Text("Return"),
-                onPressed: () {
-                  showModalBottomSheet<void>(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return SizedBox(
-                        height: 200,
-                        child: Center(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 10.0),
-                                child: Text(
-                                  bookTitle,
-                                  style: const TextStyle(
-                                      fontSize: 18.0,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                              ),
-                              const Padding(
-                                padding: EdgeInsets.symmetric(vertical: 10.0),
-                                child: Text(message),
-                              ),
-                              Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  OutlinedButton(
-                                    child: const Text('Cancel'),
-                                    onPressed: () => Navigator.pop(context),
-                                  ),
-                                  const SizedBox(
-                                    width: 20.0,
-                                  ),
-                                  FilledButton(
-                                    child: const Text('Continue'),
-                                    onPressed: () => Navigator.pop(context),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
