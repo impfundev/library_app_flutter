505c9e636e7abd73efb5612c5eb134d8c5717ea1
impfundev <ilhammaulana.dev@gmail.com>
2024-08-02 13:21:05 +0700

    fix: refactor

--- lib/src/providers/auth_provider.dart
+++ lib/src/providers/auth_provider.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-import 'package:flutter/foundation.dart';
 import 'package:go_router/go_router.dart';
 import 'package:http/http.dart' as http;
 
